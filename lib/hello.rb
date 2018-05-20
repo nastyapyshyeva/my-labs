c345a5be5448a29be5c676c4d8bf39b585c23d88
Nastya Pyshyeva <1808nastunia@gmail.com>
2018-05-20 23:11:42 +0300

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Nastya Pyshyeva (1808nastunia@gmail.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
